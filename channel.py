0c544e83566fdbde8379d2212c446515508e25d2
LK- <lk.codeshock@gmail.com>
2010-06-27 04:12:56 +1000

    Cleared old code, preparing for re-implementation of response processing

--- channel.py
+++ channel.py
@@ -23,20 +23,6 @@ def join ( self, channel, key = None ):
             self.rsend ( 'JOIN ' + channel + ' ' + key )
         else:
             self.rsend ( 'JOIN ' + channel )
-        data = self.recv()
-        while data.find ( '366' ) == -1:
-                data = self.recv()
-                segs = data.split()
-                if data.find ( '332' ) != -1:
-                     topic = data.split( ' ', 4) [4] [1:]
-                elif data.find ( '353' ) != -1:
-                        names = data.split( ' ', 5 ) [5]
-                        names = names [0] [1:] + names [1:]
-                        names = names.split()
-                elif segs [1] != '332' and segs [1] != '353' and segs[1] != 366:
-                        self.code = int ( data.split() [1] )
-                        return False
-        return [ topic, names ]
 def part ( self, channel, reason = None ):
     '''
     part() part's a channel, optionally a part message may be specified.
@@ -77,43 +63,19 @@ def cmode ( self, channel, modes = '' ):
                 self.rsend ( 'MODE ' + channel )
                 return self.recv().split() [4]
         else:   self.rsend ( 'MODE ' + channel + ' ' + modes )
-        data = self.recv()
-        if data.find ( 'MODE' ) == -1:
-                self.code = int ( data.split() [1] )
-                return False
-        self.buffer.append ( data )
+
         
 def banlist ( self, channel ):
         self.rsend ( 'MODE ' + channel + ' +b' )
-        data = self.recv()
         bans = []
-        while data.find ( '368' ) == -1:
-                rban = data.split()
-                if rban [1] != '367':
-                        self.code = int ( rban [1] )
-                        return False
-                bans.append ( rban [4] )
-
+ 
 def exceptlist ( self, channel ):
         self.rsend ( 'MODE ' + channel + ' +e' )
-        data = self.recv()
         excepts = []
-        while data.find ( '349' ) == -1:
-                rexcept = data.split()
-                if rexcept [1] != '348':
-                        self.code = int ( rexcept [1] )
-                        return False
-                excepts.append ( rexcept [4] )
+ 
 def invitelist ( self, channel ):
         self.rsend ( 'MODE ' + channel + ' +i' )
-        data = self.recv()
         invites = []
-        while data.find ( '347' ) == -1:
-                rinvites = data.split()
-                if rinvites [1] != '346':
-                        self.code = int ( rinvites [1] )
-                        return False
-                rinvites.append ( rinvites [4] )
 
 def topic ( self, channel, rtopic = None ):
         '''
@@ -133,15 +95,7 @@ def topic ( self, channel, rtopic = None ):
         else:
             self.rsend ( 'TOPIC ' + channel )
         topic = ''
-        data = self.recv()
-        while data.find ( '333' ) == -1:
-                data = self.recv()
-                if data.find ( '332' ) != -1:
-                        topic = data.split( ' ', 4 ) [4] [1:]
-                else:
-                        self.code = int ( data.split() [1] )
-                        return False
-        return topic
+
 def names ( self, channel ):
         '''
         names(), accepts one argument the name of the channel to list the nicks in, it returns a list of the nicks in the specified channel;
@@ -154,17 +108,7 @@ def names ( self, channel ):
                 }
         self.rsend ( 'NAMES ' + channel )
         names = []
-        data = self.recv()
-        while data.find ( '366' ) == -1:
-                data = self.recv()
-                if data.find ( '353' ) != -1:
-                        names = data.split( ' ', 5 ) [5]
-                        names = names [0] [1:] + names [1:]
-                        names = names.split()
-                else:
-                        self.code = int ( data.split() [1] )
-                        return False
-        return names
+  
 def slist ( self ):
         '''
         slist(), Runs a LIST on the server.;
@@ -178,21 +122,6 @@ def slist ( self ):
         
         self.rsend ( 'LIST' )
         list_info = { }
-        data = self.recv()
-        while data.find ( '323' ) == -1:
-                data = self.recv()
-                if data.find ( '322' ) != -1:
-                        p = data.split ( ' ', 6 )
-                        channel = p [3]
-                        count = p [4]
-                        modes = p [5] [3:]
-                        modes = modes [:-1]
-                        topic = p [6]
-                        list_info [ channel ] = [ count, modes, topic ]
-                else:
-                        self.code = int ( data.split() [1] )
-                        return False
-        return list_info
 def invite ( self, channel, nick ):
         replies = {
                 461 : 'ERR_NEEDMOREPARAMS',
@@ -203,10 +132,6 @@ def invite ( self, channel, nick ):
                 301 : 'RPL_AWAY'
                 }
         self.rsend ( 'INVITE ' + nick + ' ' + channel )
-        data = self.recv()
-        if data.find ( '341' ) == -1:
-                self.code = int ( data.split() [1] )
-                return False
 def kick ( self, channel, nick, reason = '' ):
         replies = {
                 461 : 'ERR_NEEDMOREPARAMS',
@@ -217,7 +142,3 @@ def kick ( self, channel, nick, reason = '' ):
                 442 : 'ERR_NOTONCHANNEL'
                 }
         self.rsend ( 'KICK ' + channel + ' ' + nick + ' :' + reason )
-        data = self.recv()
-        if data.find ( 'KICK' ) == -1:
-            self.code = int ( data.split() [1] )
-            return False
