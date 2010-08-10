06490776056c44e1eff8af4843f97649690ae4ce
LK- <lk.codeshock@gmail.com>
2010-08-10 16:14:51 +1000

    Fixed indentation error in test.py

--- src/test.py
+++ src/test.py
@@ -1,11 +1,11 @@
 import __init__
 irc = __init__.irc()
 irc.init ( 'localhost', 6667, 'LurkTest', 'lurklib', 'lurklib' )
-        print ( 'Connected' )
-        #print ( self.con_msg )
-        for x in irc.buffer: print ( x )
-        while 1:
-            try: print ( irc.recv() )
-            except KeyboardInterrupt:
-                irc.disconnect()
-                break
+print ( 'Connected' )
+#print ( self.con_msg )
+for x in irc.buffer: print ( x )
+while 1:
+    try: print ( irc.recv() )
+    except KeyboardInterrupt:
+        irc.disconnect()
+        break
