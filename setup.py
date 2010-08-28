719f1ad8cd69061770984011be49537d27f53425
LK- <lk.codeshock@gmail.com>
2010-08-28 15:20:49 +1000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ setup ( \
         name = 'lurklib',
         packages = ['lurklib'],
         version = '0.3',
+        author = 'LK-',
+        author_email = 'lk.codeshock@gmail.com',
         description = 'The Lurk Internet Relay Chat Library',
         url = 'http://codeshock.org/'
         )
