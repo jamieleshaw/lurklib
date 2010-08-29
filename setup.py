7a8a4f8823355836cf018326d2c48ef5e0df4b7a
LK- <lk.codeshock@gmail.com>
2010-08-29 15:48:26 +1000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup ( \
         name = 'lurklib',
         packages = ['lurklib'],
-        version = '0.3.2',
+        version = '0.3.3',
         author = 'LK-',
         author_email = 'lk.codeshock@gmail.com',
         description = 'The Lurk Internet Relay Chat Library',
