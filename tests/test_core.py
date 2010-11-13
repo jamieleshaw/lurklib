f1adf78b6778ce1649acaeffb27929fe1d85a638
LK- <jamieleshaw@gmail.com>
2010-11-13 16:00:18 +1100

    Fixed a bug in _recv() and added a unittest for it.

--- tests/test_core.py
+++ tests/test_core.py
@@ -20,13 +20,20 @@ class Test_Core(CommonBase):
     def test_readable(self):
         pass
 
+    def test_mcon(self):
+        pass
+
     def test__recv_(self):
         self.server_rsend('ABCD')
         self.assertEqual(self.client._recv_(), 'ABCD')
+        self.assertEqual(1, self.client._index)
+        self.assertEqual('ABCD', self.client._buffer[0])
 
     def test__recv(self):
-        pass
-
+        self.server_rsend(':Test PRIVMSG #Test :A B C D')
+        self.assertEqual(self.client._recv(), [':Test', 'PRIVMSG', '#Test', ':A B C D'])
+        self.server_rsend(':Test PRIVMSG #Test :A B C D')
+        self.assertEqual(self.client._recv(True), [':Test', 'PRIVMSG', '#Test', 'A B C D'])
     def test_send(self):
         pass
 
